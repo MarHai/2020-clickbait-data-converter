7a30bc968d1a8f38b1a84a79e75f76a3e41f87c1
marhai <mario@haim.it>
2020-05-07 11:49:37 +0200

    remove python aggregation and fix R aggregation

--- aggregate.r
+++ aggregate.r
@@ -27,27 +27,36 @@ posts <-
   select(post_id = id, link = external_link, publication_time, headline, excerpt)
 
 lct <- Sys.getlocale('LC_TIME')
-Sys.setlocale('LC_TIME', 'English')
-articles %>% 
-  left_join(posts, by = 'link', suffix = c('.article', '.post')) %>% 
-  filter(!is.na(post_id)) %>% 
-  mutate(
-    id = str_glue('{article_id}_{post_id}'),
-    postTimestamp = format(publication_time, '%a %b %d %H:%M:%S %z %Y'),
-    headline.post = str_replace_all(headline.post, '"', '\\\\"'),
-    excerpt.post = str_replace_all(excerpt.post, '"', '\\\\"'),
-    headline.article = str_replace_all(headline.article, '"', '\\\\"'),
-    excerpt.article = str_replace_all(excerpt.article, '"', '\\\\"'),
-    json = str_glue('{{"id":"{id}",',
-                    '"postTimestamp":"{postTimestamp}",',
-                    '"postText":["{headline.post}","{excerpt.post}"],',
-                    '"postMedia":[],',
-                    '"targetTitle":"{headline.article}",',
-                    '"targetDescription":"{excerpt.article}",',
-                    '"targetKeywords":"",',
-                    '"targetParagraphs":[],',
-                    '"targetCaptions":[]}}')
-  ) %>% 
-  pull(json) %>% 
-  write_lines('instances.jsonl')
+Sys.setlocale('LC_TIME', 'en_US.UTF-8')
+
+countries <- unique(articles %>% pull(country))
+for(country_code in countries) {
+  articles_temp <-
+    articles %>%
+    filter(country == country_code)
+  articles_temp %>%
+    left_join(posts, by = 'link', suffix = c('.article', '.post')) %>%
+    filter(!is.na(post_id)) %>%
+    mutate(
+      id = str_glue('{article_id}_{post_id}'),
+      postTimestamp = format(publication_time, '%a %b %d %H:%M:%S %z %Y'),
+      headline.post = str_replace_all(headline.post, '"', '\\\\"'),
+      excerpt.post = str_replace_all(excerpt.post, '"', '\\\\"'),
+      headline.article = str_replace_all(headline.article, '"', '\\\\"'),
+      excerpt.article = str_replace_all(excerpt.article, '"', '\\\\"'),
+      json = str_glue('{{"id":"{id}",',
+                      '"postTimestamp":"{postTimestamp}",',
+                      '"postText":["{headline.post}","{excerpt.post}"],',
+                      '"postMedia":[],',
+                      '"targetTitle":"{headline.article}",',
+                      '"targetDescription":"{excerpt.article}",',
+                      '"targetKeywords":"",',
+                      '"targetParagraphs":[],',
+                      '"targetCaptions":[]}}')
+    ) %>%
+    pull(json) %>%
+    write_lines('instances.jsonl',
+                append = TRUE)
+}
+
 Sys.setlocale('LC_TIME', lct)
